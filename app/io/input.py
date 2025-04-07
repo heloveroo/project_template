728b7f0c8a2d6a2a592dc780d4d3fd634a6488a5
admin <v.zinchenko@ukma.edu.ua>
2025-04-07 15:53:22 +0300

    fix error

--- app/io/input.py
+++ app/io/input.py
@@ -1,19 +1,27 @@
 def input_from_console():
     """
     Функція для вводу тексту з консолі.
+    Повертає:
+        str: Текст, введений користувачем.
     """
-    pass
+    return input("Введіть текст: ")
 
 
-def read_from_file_builtin():
+def read_from_file_builtin(filename):
     """
     Функція для зчитування з файлу за допомогою вбудованих можливостей Python.
+    Повертає:
+        str: Вміст файлу у вигляді рядка.
     """
-    pass
+    with open(filename, 'r', encoding='utf-8') as file:
+        return file.read()
 
 
-def read_from_file_pandas():
+def read_from_file_pandas(filename):
     """
     Функція для зчитування з файлу за допомогою бібліотеки pandas.
+    Повертає:
+        pandas.DataFrame: Дані з файлу у вигляді таблиці (DataFrame).
     """
-    pass
+    import pandas as pd
+    return pd.read_csv(filename)
--- app/io/output.py
+++ app/io/output.py
@@ -1,12 +1,20 @@
-def output_to_console():
+def output_to_console(text):
     """
     Функція для виводу тексту у консоль
+
+    Аргументи:
+        text (str): Текст для виводу
     """
-    pass
+    print(text)
 
 
-def write_to_file_builtin():
+def write_to_file_builtin(filename, content):
     """
     Функція для запису до файлу за допомогою вбудованих можливостей Python
+
+    Аргументи:
+        filename (str): Шлях до файлу
+        content (str): Вміст для запису
     """
-    pass
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(content)
